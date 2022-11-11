00dbd0b24c685c8861eb57a91f6dd9fe9b352446
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-11 09:10:22 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animated_login_signup_page_in_flutter/Auth_Screen.dart';
+import 'package:animated_login_signup_page_in_flutter/widgets/NewDesign.dart';
 import 'package:flutter/material.dart';
 
 void main() {
