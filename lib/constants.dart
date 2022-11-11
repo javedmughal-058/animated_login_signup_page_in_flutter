229b08bcd34b86ed0c42fc184d4b8e55ae730789
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-11-11 09:10:24 +0500

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 const Color login_bg = Color(0xff24577a);
 const Color signup_bg = Color(0xFFA85E07);
 
-const double defpaultPadding = 16.0;
+
+// const Color login_bg = Color(0xFF00C470);
+// const Color signup_bg = Color(0xFF000A54);
+const double defaultPadding = 16.0;
 const Duration defaultDuration = Duration(milliseconds: 300);
